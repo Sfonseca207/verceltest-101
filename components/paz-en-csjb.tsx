dbb2749eb7aee751fa5e83f0c7f38983add7f1ca
Emanuel Isaac Afanador Pacheco <eiafanad@bancolombia.com.co>
2024-09-16 09:25:44 -0500

    remove unused variables

--- components/paz-en-csjb.tsx
+++ components/paz-en-csjb.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import { useState } from 'react'
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
 import { Mail, Phone, MapPin, Menu, X } from "lucide-react"
 import Link from "next/link"
 
